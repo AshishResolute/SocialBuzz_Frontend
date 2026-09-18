4c40092aeeee1d51095817db2cf9ba5c14e7cbda
AshishResolute <anshugourh21@gmail.com>
2026-09-18 13:13:59 +0530

    Auth background image src issue in vercel

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,9 +1,9 @@
 import { LoginForm } from "../components/auth/loginForm.jsx";
 import { SignupForm } from "../components/auth/signupForm.jsx";
-
+import socialBuzzAuth from "../assets/socialBuzzAuth.jpg";
 export const Auth = ({ mode }) => {
   return (
-    <div className="bg-[url(../../src/assets/socialBuzzAuth.jpg)] h-screen w-screen bg-cover bg-center bg-no-repeat font-sora p-8 flex flex-col overflow-auto">
+    <div className=" h-screen w-screen bg-cover bg-center bg-no-repeat font-sora p-8 flex flex-col overflow-auto" style={{ backgroundImage: `url(${socialBuzzAuth})` }}>
       <h1 className="text-3xl font-bold text-slate-400">
         Social<span className="text-3xl font-bold text-yellow-300">Buzz</span>
       </h1>
