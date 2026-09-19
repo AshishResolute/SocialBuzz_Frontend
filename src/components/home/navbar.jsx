acd9c38a9cfaa1cca4dc21586c867794dcbb78b9
AshishResolute <anshugourh21@gmail.com>
2026-09-19 17:21:14 +0530

    Re-structured the nav bar elments as per mock design

--- src/components/home/navbar.jsx
+++ src/components/home/navbar.jsx
@@ -1,24 +1,26 @@
 import { FiSearch } from "react-icons/fi";
 import { GoBell } from "react-icons/go";
-import { useRef } from "react";
+
 
 export const NavBar = () => {
-  const searchRef = useRef(null);
   return (
-    <nav className="p-4   flex justify-evenly items-center w-full ">
-      <h1 className="text-3xl w-3/4 text-center  font-bold text-slate-400">
+    <nav className="p-4 flex justify-between sm:justify-between  items-center w-full gap-5 text-white">
+      <h1 className="text-3xl text-center inline-block font-bold text-slate-400">
         Social<span className="text-3xl font-bold text-yellow-300">Buzz</span>
       </h1>
-      <div className="flex items-center gap-4 md:gap-3 text-white ">
-        <label htmlFor="search" className="cursor-pointer"><FiSearch className="pointer-events-none text-xl"/></label> 
+      <div className="flex items-center gap-5">
+        <label htmlFor="search" className="cursor-pointer">
+          <FiSearch className="pointer-events-none text-xl" />
+        </label>
         <input
-          className="hidden sm:block p-2 rounded-2xl focus:outline-none focus:ring-2 focus:ring-yellow-300"
+          className="hidden sm:block p-1.5 rounded-2xl focus:outline-none focus:ring-2 focus:ring-yellow-300 w-80"
           type="text"
           placeholder="Search here..."
           id="search"
         />
-        <GoBell className="text-xl" />
+        <GoBell className="text-xl sm:hidden" />
       </div>
+      <GoBell className="text-xl hidden sm:block" />
     </nav>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { NavBar } from "../components/home/navbar"
 import socialBuzzAuth from '../assets/socialBuzzAuth.jpg'
 export const Home = ()=>{
     return (
-        <div className="text-black font-sora bg-center bg-cover overflow-auto bg-no-repeat h-screen w-screen" style={{backgroundImage:`url(${socialBuzzAuth})`}}>
+        <div className="text-black bg-[#0B0F19] font-sora bg-center bg-cover overflow-auto bg-no-repeat h-screen w-screen " >
             <NavBar/>
         </div>
     )
