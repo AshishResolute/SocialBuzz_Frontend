00bcf735c635310b54b5d01f3469f7983bc2ac12
AshishResolute <anshugourh21@gmail.com>
2026-09-19 16:35:45 +0530

    Added navbar to home page reponsive from 300px to more

--- src/components/home/navbar.jsx
+++ src/components/home/navbar.jsx
@@ -1,9 +1,24 @@
+import { FiSearch } from "react-icons/fi";
+import { GoBell } from "react-icons/go";
+import { useRef } from "react";
+
 export const NavBar = () => {
+  const searchRef = useRef(null);
   return (
-    <nav>
-      <h1 className="text-3xl font-bold text-slate-400">
+    <nav className="p-4   flex justify-evenly items-center w-full ">
+      <h1 className="text-3xl w-3/4 text-center  font-bold text-slate-400">
         Social<span className="text-3xl font-bold text-yellow-300">Buzz</span>
       </h1>
+      <div className="flex items-center gap-4 md:gap-3 text-white ">
+        <label htmlFor="search" className="cursor-pointer"><FiSearch className="pointer-events-none text-xl"/></label> 
+        <input
+          className="hidden sm:block p-2 rounded-2xl focus:outline-none focus:ring-2 focus:ring-yellow-300"
+          type="text"
+          placeholder="Search here..."
+          id="search"
+        />
+        <GoBell className="text-xl" />
+      </div>
     </nav>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
-const Home = ()=>{
+import { NavBar } from "../components/home/navbar"
+import socialBuzzAuth from '../assets/socialBuzzAuth.jpg'
+export const Home = ()=>{
     return (
-        <div>
-            
+        <div className="text-black font-sora bg-center bg-cover overflow-auto bg-no-repeat h-screen w-screen" style={{backgroundImage:`url(${socialBuzzAuth})`}}>
+            <NavBar/>
         </div>
     )
 }
