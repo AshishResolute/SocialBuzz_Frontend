a7e654cbdaa9eb84335d78a5896d466e9d3a60ec
AshishResolute <anshugourh21@gmail.com>
2026-09-19 13:51:02 +0530

    Home page initial setup with navbar

---
+++ src/components/home/navbar.jsx
@@ -0,0 +1,9 @@
+export const NavBar = () => {
+  return (
+    <nav>
+      <h1 className="text-3xl font-bold text-slate-400">
+        Social<span className="text-3xl font-bold text-yellow-300">Buzz</span>
+      </h1>
+    </nav>
+  );
+};
